d28413d3555aafa0318e7e2b65ea5e66b9eab16e
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-10-12 10:08:59 +0700

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+        {
+            name: "",
+            script: "npm run start:deploy",
+            env: {
+
+            }
+        }
+    ],
+    deploy: {
+        dev: {
+            host: "",
+            port: "",
+            user: "",
+            repo: "",
+            ref: "",
+            path: "",
+            env: {
+
+            },
+            "post-deploy": ""
+        }
+    }
+}
